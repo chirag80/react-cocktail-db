12316bb4984523a00691b1ff301168848733833b
Patel, Chirag (cp121p) <cp121p@att.com>
2019-12-18 16:32:02 -0500

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi repellat
+        asperiores id cupiditate. Ut ad quis sit aperiam, est suscipit eligendi
+        eaque maxime delectus beatae, esse iste tempore reiciendis modi! Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Possimus non quam
+        fugit maiores ab repudiandae laboriosam. Asperiores itaque dolor,
+        voluptatum eaque sunt, cupiditate ut ratione magnam corrupti aperiam
+        quod iusto nesciunt excepturi qui laboriosam fugiat dolore blanditiis
+        recusandae tempora! Libero incidunt doloremque quasi optio velit sit
+        inventore asperiores omnis sequi?
+      </p>
+    </section>
+  );
+}
